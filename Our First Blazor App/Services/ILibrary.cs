c7176cdacd43281d8df888e0f7a6b2d55b4ea4ff
Levi Shingleton <levishing@gmail.com>
2023-11-14 14:56:27 -0500

    Initial commit

---
+++ Our First Blazor App/Services/ILibrary.cs
@@ -0,0 +1,11 @@
+﻿using BlazorConversion.Data;
+
+namespace BlazorConversion.Services
+{
+    public interface ILibrary
+    {
+        public List<User> Users { get; set; }
+        public List<Book> Books { get; set; }
+        public Dictionary<User, List<Book>> borrowedBooks { get; set; }
+    }
+}
---
+++ Our First Blazor App/Services/Library.cs
@@ -0,0 +1,11 @@
+﻿using BlazorConversion.Data;
+
+namespace BlazorConversion.Services
+{
+    public class Library : ILibrary
+    {
+        public List<User> Users { get; set; } = new List<User>();
+        public List<Book> Books { get; set; } = new List<Book>();
+        public Dictionary<User, List<Book>> borrowedBooks { get; set ; } = new Dictionary<User, List<Book>>();
+    }
+}
